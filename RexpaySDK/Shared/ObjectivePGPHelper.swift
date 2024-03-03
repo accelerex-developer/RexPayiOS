3b1edb4949c4897ae42439dd4c6939ddd7dc61c2
= <abdullahijimoh3.ja@gmail.com>
2024-03-03 14:15:03 +0100

    Add implementation that allow client to pass all necessary keys

--- RexpaySDK/Shared/ObjectivePGPHelper.swift
+++ RexpaySDK/Shared/ObjectivePGPHelper.swift
@@ -18,13 +18,8 @@ final class ObjectivePGPHelper {
     
     func encrypt(params: [String: Any], addSignature: Bool, passphraseForKey: ((Key) -> String?)? = nil) async throws -> String? {
         do {
-//            let data = try NSKeyedArchiver.archivedData(withRootObject: params, requiringSecureCoding: false)
-
+            
             let data = try? JSONSerialization.data(withJSONObject: params, options: [])
-            
-            print("loko is \(data)")
-            
-//            let publicKeyData = try Data(contentsOf: URL(fileURLWithPath: getRexpayPublicKeyPath()!))
             
             let publicKeyData = try Data(contentsOf: URL(fileURLWithPath: config!.rexpayPublicKeyPath))
 
@@ -32,25 +27,18 @@ final class ObjectivePGPHelper {
             let keys = try ObjectivePGP.readKeys(from: publicKeyData)
             print("keys is \(keys)")
             
-            
             let encryptedData = try ObjectivePGP.encrypt(data!, addSignature: addSignature, using: keys, passphraseForKey: passphraseForKey)
             
             let encryptedDataString = Armor.armored(encryptedData, as: .message)
-            let dddd = Armor.isArmoredData(encryptedData)
-            print("encrypt2 encryptedDataString is \(encryptedDataString)")
-            print("dddd  is \(dddd)")
+            let isArmoredData = Armor.isArmoredData(encryptedData)
             
-//            let signature = try ObjectivePGP.sign(encryptedData, detached:false, using: [keys.first!])
-//
-//            try ObjectivePGP.verify(encryptedData, withSignature: signature, using: [keys.first!])
+            print("encrypt encryptedDataString is \(encryptedDataString)")
+            print("isArmoredData  is \(isArmoredData)")
             
-//            let encryptedData2 = try ObjectivePGP.sign(encryptedData, detached: true, using: [keys.first!])
-            
-        
             return encryptedDataString
         }
         catch {
-            print("encrypt2 => \(error.localizedDescription)")
+            print("encrypt => \(error.localizedDescription)")
             return error.localizedDescription
         }
     }
@@ -61,15 +49,13 @@ final class ObjectivePGPHelper {
             let encryptedData = encryptedStringResponse.data(using: .utf8)
             
             let isArmoredData = Armor.isArmoredData(encryptedData!)
-            print("decrypt2 isArmoredData is \(isArmoredData)")
             
+            print("decrypt isArmoredData is \(isArmoredData)")
             print("clientPrivateKey is \(config?.clientPrivateKeyPath)")
-//            let privateKeyData = try Data(contentsOf: URL(fileURLWithPath: clientPrivateKey!))
             
             let privateKeyData = try Data(contentsOf: URL(fileURLWithPath: config!.clientPrivateKeyPath))
             
             var key = try ObjectivePGP.readKeys(from: privateKeyData).first
-            //var decryptedKey: Key?
             
             print("keys.first?.secretKey?.isEncryptedWithPassword is \(key?.secretKey?.isEncryptedWithPassword)")
             
@@ -78,77 +64,28 @@ final class ObjectivePGPHelper {
                     key = try key?.decrypted(withPassphrase: withPassphrase)
                 }
             }
-            //pgptool77@@
             
-            print("decrypt2 encryptedData is \(encryptedData)")
-            
-    
+            print("decrypt encryptedData is \(encryptedData)")
             
             let decryptedData = try ObjectivePGP.decrypt(encryptedData!, andVerifySignature: andVerifySignature, using: [key!], passphraseForKey: passphraseForKey)
             
             
             let encryptedDataString = Armor.armored(encryptedData!, as: .message)
-            print("decrypt2 encryptedDataString is \(encryptedDataString)")
+            print("decrypt encryptedDataString is \(encryptedDataString)")
             
             let res = String(data: decryptedData, encoding: .utf8)
             
             print("decrypted string \(res)")
             
-            var decodedResponse = try? JSONDecoder().decode(T.self, from: decryptedData)
+            let decodedResponse = try JSONDecoder().decode(T.self, from: decryptedData)
             
             return decodedResponse
         }
         catch {
-            print("decrypt2 ObjectivePGPHelper decrpt error => \(error.localizedDescription)")
+            print("decrypt ObjectivePGPHelper decrpt error => \(error.localizedDescription)")
             return nil
         }
     }
-    
-//    static func getRexpayPublicKeyPath() -> String? {
-//        let bundle = Bundle(for: ObjectivePGPHelper.self)
-//        let path =  bundle.path(forResource: "Rexpay-PublicKey", ofType: "asc")
-//        print("getRexpayPublicKeyPath is \(path)")
-//        return path
-//    }
-    
-    func getRexpayPublicKeyPath() -> String? {
-       let bundle = Bundle(for: ObjectivePGPHelper.self)
-       if let publicKeyPath = bundle.path(forResource: "Rexpay-PublicKey", ofType: "asc") {
-           print("publicKeyPath is \(publicKeyPath)")
-           // Check if the file exists before returning the path
-           if FileManager.default.fileExists(atPath: publicKeyPath) {
-               print("Public Key file exist.")
-               return publicKeyPath
-           } else {
-               print("Public Key file does not exist.")
-           }
-       } else {
-           print("Public Key file not found in the bundle.")
-       }
-       return nil
-    }
-    
-//    var clientPublicKey: String? {
-//        let bundle = Bundle(for: ObjectivePGPHelper.self)
-//        if let publicKeyPath = bundle.path(forResource: "Client-pub", ofType: "asc") {
-//            // Check if the file exists before returning the path
-//            if FileManager.default.fileExists(atPath: publicKeyPath) {
-//                print("Client public Key file exist.")
-//                let publicKeyData = try? Data(contentsOf: URL(fileURLWithPath: publicKeyPath))
-//
-//                let keys = try? ObjectivePGP.readKeys(from: publicKeyData!).first?.export()
-//                let armoredKey = Armor.armored(keys!, as: .publicKey) // This is used to get the PGP format in string
-//                print("armoredKey is \(armoredKey)")
-//
-//                return armoredKey
-//            } else {
-//                print("ClientPublic Key file does not exist.")
-//            }
-//        } else {
-//            print("Client Public Key file not found in the bundle.")
-//        }
-//        return nil
-//    }
     
     var clientPublicKey: String? {
         do {
@@ -171,33 +108,6 @@ final class ObjectivePGPHelper {
         }
         catch {
             print("RexpaySDK error => \(error.localizedDescription)")
-        }
-        return nil
-    }
-    
-    var clientPrivateKey: String? {
-        let bundle = Bundle(for: ObjectivePGPHelper.self)
-        if let privateKeyPath = bundle.path(forResource: "Client-sec", ofType: "asc") {
-            // Check if the file exists before returning the path
-            if FileManager.default.fileExists(atPath: privateKeyPath) {
-                print("Client private Key file exist.")
-                let privateeyDataa = try? Data(contentsOf: URL(fileURLWithPath: privateKeyPath))
-                
-                let keys = try? ObjectivePGP.readKeys(from: privateeyDataa!).first?.export()
-                
-                let armoredKey = Armor.armored(keys!, as: .message) // This is used to get the PGP format in string
-                print(" Client armoredKey is \(armoredKey)")
-                let armoredKey2 = Armor.armored(keys!, as: .secretKey)
-                
-                print(" Client armoredKey2 is \(armoredKey2)")
-                
-                
-                return privateKeyPath
-            } else {
-                print("Client private Key file does not exist.")
-            }
-        } else {
-            print("Client private Key file not found in the bundle.")
         }
         return nil
     }
