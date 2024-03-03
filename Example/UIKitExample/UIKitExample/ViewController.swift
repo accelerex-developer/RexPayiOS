9cc5985c0a7c97d11c79ebff8d9ed81e980062db
= <abdullahijimoh3.ja@gmail.com>
2024-03-03 22:57:05 +0100

    Fix UIKit example app

--- Example/UIKitExample/UIKitExample/ViewController.swift
+++ Example/UIKitExample/UIKitExample/ViewController.swift
@@ -16,22 +16,49 @@ class ViewController: UIViewController {
     
     @IBAction func clickMe(_ sender: UIButton) {
         let config = RexpaySDKConfig(
-            reference: "sman024",
+            reference: "sman036",
             amount: 210,
             userId: "awoyeyetimilehin@gmail.com",
             email: "awoyeyetimilehin@gmail.com",
             customerName: "Victor Musa",
             username: "talk2phasahsyyahoocom",
             password: "f0bedbea93df09264a4f09a6b38de6e9b924b6cb92bf4a0c07ce46f26f85",
-            rexpayPublicKeyPath: "",
-            publicKeyPath: "",
-            privateKeyPath: "",
+            rexpayPublicKeyPath: rexpayPublicKeyPath(),
+            clientPublicKeyPath: clientPublicKeyPath(),
+            clientPrivateKeyPath: clientPrivateKeyPath(),
             delegate: self)
-        
-        config.passphrase = "pgptool77@@"
 
+        config.passphrase = "pgptool77@@"
+        
         let rexpaySDK = RexpaySDK(config: config)
         rexpaySDK.launch(presentingViewController: self)
+    }
+    
+    func rexpayPublicKeyPath() -> String {
+        if let filePath = Bundle.main.path(forResource: "Rexpay-PublicKey", ofType: "asc") {
+            return filePath
+        } else {
+            print("Rexpay-PublicKey file not found.")
+            return ""
+        }
+    }
+    
+    func clientPublicKeyPath() -> String {
+        if let filePath = Bundle.main.path(forResource: "Client-pub", ofType: "asc") {
+            return filePath
+        } else {
+            print("Client-pub file not found.")
+            return ""
+        }
+    }
+    
+    func clientPrivateKeyPath() -> String {
+        if let filePath = Bundle.main.path(forResource: "Client-sec", ofType: "asc") {
+            return filePath
+        } else {
+            print("Client-sec file not found.")
+            return ""
+        }
     }
 }
 
@@ -40,4 +67,5 @@ extension ViewController: RexpaySDKResponseDelegate {
         print("callback mesages => \(message)")
     }
 }
+
 
